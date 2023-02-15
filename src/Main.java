df354f6ae9668a8a668b1c2119fa8ee241ebecd3
Anton Pantiukhin <jobapsoch@mail.ru>
2023-02-15 20:03:39 +0300

    Добавлен класс Main

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,9 @@
 public class Main {
+  public static void main(String[] args) {
+    int ticketPrice = 40_220;
+    int oneMile = 20;
+    int summMile = ticketPrice / oneMile;
+    System.out.println("Вам начислено: " + summMile + " миль");
+  }
 
 }
